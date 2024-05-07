00857bee5f107127d2f0009abad6685ae7c917f8
p2659716 <p2659716@my365.dmu.ac.uk>
2024-05-07 12:44:25 +0100

    Practical 7 DONE

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public bool Availability { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int StaffID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
