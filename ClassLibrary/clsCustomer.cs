af9ea38e5f81260cbaa186f4fde1c66f44a777c2
p2653673 <p2653673@my365.dmu.acuk>
2024-05-07 12:57:00 +0100

    Created Methods fo the attributes and then teste them to ensure there are no errors

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public bool Subscribe { get; set; }
+        public DateTime DateJoined { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int CustomerID { get; set; }
     }
 }
