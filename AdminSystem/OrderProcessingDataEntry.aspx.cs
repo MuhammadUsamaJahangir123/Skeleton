d4d46b45f7e8decc6e7ecff4eac532829d3b5046
p2766565 <p2766565@GWHXX653WIN018W>
2024-05-08 17:29:57 +0100

    lab 8 continuation

--- AdminSystem/OrderProcessingDataEntry.aspx.cs
+++ AdminSystem/OrderProcessingDataEntry.aspx.cs
@@ -1,20 +1,39 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+       
 
     }
 
     protected void btnOK_Click(object sender, EventArgs e)
     {
-        //navigate to viewer page
+        //create a new instance of clsOrderProcessing
+        clsOrderProcessing AnOrderProcessing = new clsOrderProcessing();
+        //store the data in the session object
+
+        AnOrderProcessing.OrderID = Convert.ToInt32(txtOrderId.Text);
+        AnOrderProcessing.CustomerID = Convert.ToInt32(txtCustomerId.Text);
+        AnOrderProcessing.StaffID = Convert.ToInt32(txtStaffId.Text);
+        AnOrderProcessing.OrderDate = Convert.ToDateTime(txtOrderDate.Text);
+        AnOrderProcessing.TotalAmount = Convert.ToDecimal(txtTotalAmount.Text);
+        AnOrderProcessing.ShippingStatus = chkShippingStatus.Checked;
+
+        //navigate to the view page
+        Session["AnOrderProcessing"] = AnOrderProcessing;
+        
+        
+       
         Response.Redirect("OrderProcessingViewer.aspx");
+        
     }
 }
