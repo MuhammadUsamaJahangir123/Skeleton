cfaf7731d7482aeab108073b45959f47c6f19158
p2653673 <p2653673@my365.dmu.acuk>
2024-05-08 11:23:11 +0100

    Made a form with 2 pages working with erros and showing data.

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -4,10 +4,61 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnOK_Click(object sender, EventArgs e)
+    { 
+        clsCustomer AnCustomer = new clsCustomer();
+
+        AnCustomer.CustomerID = Convert.ToInt32(txtCustomerID.Text);
+        //capture the customer first name
+        AnCustomer.FirstName = txtFirstName.Text;
+        //capture the customer last name
+        AnCustomer.LastName = txtLastName.Text;
+        //capture the customer Email
+        AnCustomer.Email = txtEmail.Text;
+        //capture the date
+        AnCustomer.DateJoined = Convert.ToDateTime(DateTime.Now);
+        //capture if the subscribed
+        AnCustomer.Subscribe = chkSubscribe.Checked;
+        Session["AnCustomer"] = AnCustomer;
+        //navigate to the view page
+        Response.Redirect("CustomerViewer.aspx");
+
+    }
+
+    protected void txtCustomerID_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void txtFirstName_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void txtLastName_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void txtEmail_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void txtJoinedDate_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void chkSubscribe_CheckedChanged(object sender, EventArgs e)
     {
 
     }
--- AdminSystem/CustomerViewer.aspx.cs
+++ AdminSystem/CustomerViewer.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,17 @@ public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //create a new instance of clsCustomer
+        clsCustomer AnCustomer = new clsCustomer();
+        //get data from the session object
+        AnCustomer = (clsCustomer)Session["AnCustomer"];
+        //Display the name
+        Response.Write(AnCustomer.CustomerID);
+        Response.Write(AnCustomer.FirstName);
+        Response.Write(AnCustomer.LastName);
+        Response.Write(AnCustomer.Email);
+        Response.Write(AnCustomer.DateJoined);
+        Response.Write(AnCustomer.Subscribe);
 
     }
 }
