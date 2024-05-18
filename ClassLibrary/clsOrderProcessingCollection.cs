0cace52d64136d2c117fee6b476d77db9891f648
p2766565 <p2766565@GWHXX653WIN018W>
2024-05-18 17:27:30 +0100

    lab12 finished

---
+++ ClassLibrary/clsOrderProcessingCollection.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ClassLibrary
+{
+    public class clsOrderProcessingCollection
+    {
+        //private data member for the list
+        List<clsOrderProcessing> mOrderProcessingList = new List<clsOrderProcessing>();
+        public List<clsOrderProcessing> OrderProcessingList {
+            get
+            { //return the private data
+                return mOrderProcessingList;
+            }
+            set
+            {
+                // set the private data
+                mOrderProcessingList = value;
+            }
+        }
+        public int Count {
+            get
+            { //return the count of the list
+                return mOrderProcessingList.Count;
+            }
+            set
+            {
+                // 
+
+            }
+        }
+        public clsOrderProcessing ThisOrderProcessing { get; set; }
+
+        public clsOrderProcessingCollection() 
+        {
+            //variable for the index
+            Int32 Index = 0;
+            //variable to store the record count
+            Int32 RecordCount = 0;
+            //inject for the data connect
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tborders_SelectAll");
+            //get the count of records
+            RecordCount = DB.Count;
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank orderprocessing
+                clsOrderProcessing AnOrderProcessing = new clsOrderProcessing();
+                //read in the fields for the current record
+                AnOrderProcessing.OrderID = Convert.ToInt32(DB.DataTable.Rows[Index]["OrderId"]);
+                AnOrderProcessing.CustomerID = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerId"]);
+                AnOrderProcessing.StaffID = Convert.ToInt32(DB.DataTable.Rows[Index]["StaffId"]);
+                AnOrderProcessing.OrderDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["OrderDate"]);
+                AnOrderProcessing.ShippingStatus = Convert.ToBoolean(DB.DataTable.Rows[Index]["ShippingStatus"]);
+                AnOrderProcessing.TotalAmount = Convert.ToDecimal(DB.DataTable.Rows[Index]["TotalAmount"]);
+                //add the record to the private data member
+                mOrderProcessingList.Add(AnOrderProcessing);
+                //point at the next record
+                Index++;
+            }
+        }
+
+
+    }
+}
---
+++ Testing3/tstOrderProcessingCollection.cs
@@ -0,0 +1,91 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstOrderProcessingCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsOrderProcessingCollection AllOrderProcessings = new clsOrderProcessingCollection();
+            //test to see that it exists
+            Assert.IsNotNull(AllOrderProcessings);
+        }
+
+        [TestMethod]
+        public void OrderProcessingListOK()
+        {
+            //create am instance of the class we want to create
+            clsOrderProcessingCollection AllOrderProcessings = new clsOrderProcessingCollection();
+            //create some test data to assign to property
+            //in this case test the data meeds to be a list of objects
+            List<clsOrderProcessing> TestList = new List<clsOrderProcessing>();
+            //add an Item to the lsit of test data
+            clsOrderProcessing TestItem = new clsOrderProcessing();
+            //set its properties
+            TestItem.OrderID = 1;
+            TestItem.CustomerID = 2;
+            TestItem.StaffID = 3;
+            TestItem.TotalAmount = 25m;
+            TestItem.ShippingStatus = true;
+            TestItem.OrderDate = DateTime.Now;
+            //Add the item to the test list
+            TestList.Add(TestItem);
+            //Assign the data to the property
+            AllOrderProcessings.OrderProcessingList = TestList;
+            //Test to see that the two values are the same
+            Assert.AreEqual(AllOrderProcessings.OrderProcessingList, TestList);
+        }
+
+        [TestMethod]
+        public void ThisOrderProcessingPropertyOK()
+        {
+            //create am instance of the class we want to create
+            clsOrderProcessingCollection AllOrderProcessings = new clsOrderProcessingCollection();
+            //create some test data to assign to property
+            clsOrderProcessing TestOrderProcessing = new clsOrderProcessing();
+            //set the properties of the test object
+            TestOrderProcessing.OrderID = 1;
+            TestOrderProcessing.CustomerID = 2;
+            TestOrderProcessing.StaffID = 3;
+            TestOrderProcessing.TotalAmount = 25m;
+            TestOrderProcessing.ShippingStatus = true;
+            TestOrderProcessing.OrderDate = DateTime.Now;
+            //Assign the data to the property
+            AllOrderProcessings.ThisOrderProcessing = TestOrderProcessing;
+            //Test to see that the two values are the same
+            Assert.AreEqual(AllOrderProcessings.ThisOrderProcessing, TestOrderProcessing);
+        }
+        
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create am instance of the class we want to create
+            clsOrderProcessingCollection AllOrderProcessings = new clsOrderProcessingCollection();
+            //create some test data to assign to property
+            //in this case test the data meeds to be a list of objects
+            List<clsOrderProcessing> TestList = new List<clsOrderProcessing>();
+            //add an Item to the list
+            //Create the item of test data
+            clsOrderProcessing TestItem = new clsOrderProcessing();
+            //set its properties
+            TestItem.OrderID = 1;
+            TestItem.CustomerID = 2;
+            TestItem.StaffID = 3;
+            TestItem.TotalAmount = 25m;
+            TestItem.ShippingStatus = true;
+            TestItem.OrderDate = DateTime.Now;
+            //Add the item to the test list
+            TestList.Add(TestItem);
+            //Assign the data to the property
+            AllOrderProcessings.OrderProcessingList = TestList;
+            //Test to see that the two values are the same
+            Assert.AreEqual(AllOrderProcessings.Count, TestList.Count);
+        }
+    }
+}
