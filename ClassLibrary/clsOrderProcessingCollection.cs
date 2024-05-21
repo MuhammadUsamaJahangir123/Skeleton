a4d1959e59c64ae676125c0fd130ac3b7ef5b6ba
p2766565 <p2766565@GWHXX653WIN018W>
2024-05-21 19:04:46 +0100

    not finishd yet

--- ClassLibrary/clsOrderProcessingCollection.cs
+++ ClassLibrary/clsOrderProcessingCollection.cs
@@ -8,6 +8,8 @@ namespace ClassLibrary
     {
         //private data member for the list
         List<clsOrderProcessing> mOrderProcessingList = new List<clsOrderProcessing>();
+        //private member data for thisaddress
+        clsOrderProcessing mThisOrderProcessing = new clsOrderProcessing();
         public List<clsOrderProcessing> OrderProcessingList {
             get
             { //return the private data
@@ -30,7 +32,17 @@ namespace ClassLibrary
 
             }
         }
-        public clsOrderProcessing ThisOrderProcessing { get; set; }
+        public clsOrderProcessing ThisOrderProcessing {
+            get
+            { //return the private data
+                return mThisOrderProcessing;
+            }
+            set
+            {
+                // set the private data
+                mThisOrderProcessing = value;
+            }
+        }
 
         public clsOrderProcessingCollection() 
         {
@@ -56,6 +68,7 @@ namespace ClassLibrary
                 AnOrderProcessing.OrderDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["OrderDate"]);
                 AnOrderProcessing.ShippingStatus = Convert.ToBoolean(DB.DataTable.Rows[Index]["ShippingStatus"]);
                 AnOrderProcessing.TotalAmount = Convert.ToDecimal(DB.DataTable.Rows[Index]["TotalAmount"]);
+                AnOrderProcessing.ShippingAddress = Convert.ToString(DB.DataTable.Rows[Index]["ShippingAddress"]);
                 //add the record to the private data member
                 mOrderProcessingList.Add(AnOrderProcessing);
                 //point at the next record
@@ -63,6 +76,20 @@ namespace ClassLibrary
             }
         }
 
+        public int Add()
+        {
+            //adds a record to the database based on the values of mthisOrderProcessing
+            //set the primary key value of the new record
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("@CustomrId", mThisOrderProcessing.CustomerID);
+            DB.AddParameter("@StaffId", mThisOrderProcessing.StaffID);
+            DB.AddParameter("@OrderDate", mThisOrderProcessing.OrderDate);
+            DB.AddParameter("@ShippingStatus", mThisOrderProcessing.ShippingStatus);
+            DB.AddParameter("@ShippingAddress", mThisOrderProcessing.ShippingAddress);
+            DB.AddParameter("@TotalAmount", mThisOrderProcessing.TotalAmount);
+            return DB.Execute("sproc_tborders_Insert");
+        }
 
     }
 }
