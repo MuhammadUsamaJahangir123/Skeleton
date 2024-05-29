bff4c9d2688d7e5ef5c5b90bc476df1a972a76cc
P2676131 <P2676131@CLPX0027WIN017W>
2024-05-29 11:44:42 +0100

    Another one

--- AdminSystem/SupplierList.aspx.cs
+++ AdminSystem/SupplierList.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using ClassLibrary;
+
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
@@ -60,6 +61,28 @@ public partial class _1_List : System.Web.UI.Page
         {
             lblError.Text = "Please select a record from the list to delete";
         }
+
+    }
+
+    protected void Applybtn_Click(object sender, EventArgs e)
+    {
+        clsSupplierCollection ASupplier = new clsSupplierCollection();
+        ASupplier.ReportByPostCode(txtFilter.Text);
+        lstSupplierList.DataSource = ASupplier.SupplierList;
+        lstSupplierList.DataValueField = "SupplierId";
+        lstSupplierList.DataTextField = "SupplierPostCode";
+        lstSupplierList.DataBind();
+    }
+
+    protected void Clearbtn_Click(object sender, EventArgs e)
+    {
+        clsSupplierCollection ASupplier = new clsSupplierCollection();
+        ASupplier.ReportByPostCode("");
+        txtFilter.Text = "";
+        lstSupplierList.DataSource = ASupplier.SupplierList;
+        lstSupplierList.DataValueField = "SupplierId";
+        lstSupplierList.DataTextField = "SupplierPostCode";
+        lstSupplierList.DataBind();
 
     }
 }
